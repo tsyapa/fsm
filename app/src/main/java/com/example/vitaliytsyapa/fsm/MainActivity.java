d592e07678ebbebce03760446ef26ee301f4aa42
tsyapa <tsyapper14@gmail.com>
2017-10-17 17:19:41 +0300

    edit layout

--- app/src/main/java/com/example/vitaliytsyapa/fsm/MainActivity.java
+++ app/src/main/java/com/example/vitaliytsyapa/fsm/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.vitaliytsyapa.fsm;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final TextView state=(TextView) findViewById(R.id.textView);
+        Button btnLock=(Button) findViewById(R.id.buttonLock);
+        btnLock.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                state.setBackgroundColor(getResources().getColor(R.color.red));
+            }
+        });
     }
 }
