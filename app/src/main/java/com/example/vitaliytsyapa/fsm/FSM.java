90f2419be5c513c6e0052fe51018b722609ef125
tsyapa <tsyapper14@gmail.com>
2017-10-17 19:58:12 +0300

    implement FSM, State and Action

---
+++ app/src/main/java/com/example/vitaliytsyapa/fsm/FSM.java
@@ -0,0 +1,37 @@
+package com.example.vitaliytsyapa.fsm;
+
+/**
+ * Created by Vitaliy Tsyapa on 10/17/2017.
+ */
+
+public class FSM {
+
+    private int[] states={State.ALARM_DISARMED_ALL_UNLOCKED,State.ALARM_DISARMED_ALL_LOCKED,
+            State.ALARM_ARMED_ALL_LOCKED,State.ALARM_DISARMED_DRIVER_UNLOCKED};
+    //lock, lockx2, unlock, unlockx2
+    private int[][] transitions={{State.ALARM_DISARMED_ALL_LOCKED,State.ALARM_ARMED_ALL_LOCKED,State.ALARM_DISARMED_ALL_UNLOCKED,State.ALARM_DISARMED_ALL_UNLOCKED},
+            {State.ALARM_ARMED_ALL_LOCKED,State.ALARM_DISARMED_ALL_UNLOCKED,State.ALARM_DISARMED_DRIVER_UNLOCKED,State.ALARM_DISARMED_ALL_UNLOCKED},
+            {State.ALARM_ARMED_ALL_LOCKED,State.ALARM_ARMED_ALL_LOCKED,State.ALARM_DISARMED_DRIVER_UNLOCKED,State.ALARM_DISARMED_ALL_UNLOCKED},
+            {State.ALARM_DISARMED_ALL_LOCKED,State.ALARM_ARMED_ALL_LOCKED,State.ALARM_DISARMED_DRIVER_UNLOCKED,State.ALARM_DISARMED_DRIVER_UNLOCKED}};
+    private int currentState=State.ALARM_DISARMED_ALL_UNLOCKED;
+
+    private void changeState(int action){
+        currentState=transitions[currentState][action];
+    }
+
+    public void lock(){
+        changeState(Action.LOCK);
+    }
+
+    public void lockX2(){
+        changeState(Action.LOCK_X2);
+    }
+
+    public void unlock(){
+        changeState(Action.UNLOCK);
+    }
+
+    public void unlockX2(){
+        changeState(Action.UNLOCK_X2);
+    }
+}
