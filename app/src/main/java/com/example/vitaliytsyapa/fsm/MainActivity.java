3fdcb8a844206b452c69f5a62ef9f10a81e43d52
tsyapa <tsyapper14@gmail.com>
2017-10-17 16:42:31 +0300

    edit strings

--- app/src/main/java/com/example/vitaliytsyapa/fsm/MainActivity.java
+++ app/src/main/java/com/example/vitaliytsyapa/fsm/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.vitaliytsyapa.fsm;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
