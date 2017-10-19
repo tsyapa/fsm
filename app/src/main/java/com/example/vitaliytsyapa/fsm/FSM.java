77987c50b0cd70fa7177a98e44d8048c7df31307
tsyapa <tsyapper14@gmail.com>
2017-10-19 15:31:41 +0300

    create assets folder and edit FSM class

--- app/src/main/java/com/example/vitaliytsyapa/fsm/FSM.java
+++ app/src/main/java/com/example/vitaliytsyapa/fsm/FSM.java
@@ -1,37 +1,22 @@
 package com.example.vitaliytsyapa.fsm;
 
+import java.util.ArrayList;
+
 /**
  * Created by Vitaliy Tsyapa on 10/17/2017.
  */
 
 public class FSM {
 
-    private int[] states={State.ALARM_DISARMED_ALL_UNLOCKED,State.ALARM_DISARMED_ALL_LOCKED,
-            State.ALARM_ARMED_ALL_LOCKED,State.ALARM_DISARMED_DRIVER_UNLOCKED};
-    //lock, lockx2, unlock, unlockx2
-    private int[][] transitions={{State.ALARM_DISARMED_ALL_LOCKED,State.ALARM_ARMED_ALL_LOCKED,State.ALARM_DISARMED_ALL_UNLOCKED,State.ALARM_DISARMED_ALL_UNLOCKED},
-            {State.ALARM_ARMED_ALL_LOCKED,State.ALARM_DISARMED_ALL_UNLOCKED,State.ALARM_DISARMED_DRIVER_UNLOCKED,State.ALARM_DISARMED_ALL_UNLOCKED},
-            {State.ALARM_ARMED_ALL_LOCKED,State.ALARM_ARMED_ALL_LOCKED,State.ALARM_DISARMED_DRIVER_UNLOCKED,State.ALARM_DISARMED_ALL_UNLOCKED},
-            {State.ALARM_DISARMED_ALL_LOCKED,State.ALARM_ARMED_ALL_LOCKED,State.ALARM_DISARMED_DRIVER_UNLOCKED,State.ALARM_DISARMED_DRIVER_UNLOCKED}};
-    private int currentState=State.ALARM_DISARMED_ALL_UNLOCKED;
-    //private ArrayList<Transition>=new ArrayList<>;
-    private void changeState(int action){
-        currentState=transitions[currentState][action];
+    private ArrayList<String> states;
+    private ArrayList<String> actions;
+    private String initialState;
+
+    public FSM(){
+        init();
     }
 
-    public void lock(){
-        changeState(Action.LOCK);
-    }
-
-    public void lockX2(){
-        changeState(Action.LOCK_X2);
-    }
-
-    public void unlock(){
-        changeState(Action.UNLOCK);
-    }
-
-    public void unlockX2(){
-        changeState(Action.UNLOCK_X2);
+    private void init(){
+        
     }
 }
